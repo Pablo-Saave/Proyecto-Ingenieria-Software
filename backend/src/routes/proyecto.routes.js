7250c100fe26fadb36ec0f5015777ca1363a79b1
FranciscoDiazC <francisco.diaz1901@alumnos.ubiobio.cl>
2026-07-03 00:42:54 -0400

    refactor(proyecto): actualizar rutas y controladores
    - Reorganizar las rutas y controladores del módulo Proyectos para
      adaptarlos a la nueva arquitectura y mejorar la separación de
      responsabilidades

--- backend/src/routes/proyecto.routes.js
+++ backend/src/routes/proyecto.routes.js
@@ -3,17 +3,24 @@
 import { Router } from "express";
 import { autorizar } from "../middlewares/autorizar.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { validarCrearProyecto } from "../validations/proyectos.validation.js";
 
 import {
-  getProyectos,
+  validarActualizarProyecto,
+  validarCambiarSupervisorProyecto,
+  validarCrearProyecto,
+  validarInactivarProyecto,
+  validarReactivarProyecto,
+  validarRemoverSupervisorDeProyecto
+} from "../validations/proyectos.validation.js";
+
+import {
   crearProyecto,
   actualizarProyecto,
-  getMyProyectosByToken,
-  getProyectData,
   inactivarProyecto,
   reactivarProyecto,
-  crearProyectoQuickFix
+  getAllProyectos,
+  cambiarSupervisorProyecto,
+  removerSupervisorDeProyecto
 } from "../controllers/proyecto.controller.js";
 
 
@@ -22,18 +29,18 @@ const router = Router();
 router.use(authMiddleware); // Verifica Login - Agrega info de quien consulta y permisos al request
 
 // Administrador
-/* LISTO */ //router.post("/", crearProyecto); // Crea un nuevo Proyecto (Recibe id_cliente, nombre_proyecto, tipo_instalacion, direccion, nivel_exigencia, cantidad_personal_requerido) (Validacion 1: El que crea el proyecto debe tener tipo_usuario = administrador) (Validacion 2: El cliente id_cliente debe existir) (Validacion 3: Los campos no deben estar vacios)
-/* LISTO */ router.get("/", getProyectos); // Devuelve Lista de proyectos,(junto con informacion de su respectivo cliente) [paginados y ordenados (orden por fecha (por defecto), orden alfabetico nombre_proyecto opcional debe especificarse)] (Validacion 1: El que hace la peticion debe tener tipo_usuario = administrador)
-/* LISTO */ router.patch("/:id_proyecto", actualizarProyecto); // Actualiza los datos de un proyecto  (Puede ser nombre_proyecto, tipo_instalacion, direccion, nivel_exigencia, cantidad_personal_requerido) (Validacion 1: El que hace la peticion debe tener tipo_usuario = administrador) (Validacion 2: El proyecto a actualizar debe existir) (Validacion 3: La peticion debe contener al menos un campo a actualizar)
-/* LISTO */ router.delete("/inactivar/:id", inactivarProyecto); // Cambia el estado de Proyecto a "inactivo", en cascada cambia el estado de todas sus cuadrillas a "inactiva". (Validacion 1: El que realiza la peticion debe tener tipo_usuario = administrador) (Validacion 2: Si en la cascada una cuadrilla ya se encontraba inactiva, dejarla asi)
-/* LISTO */ router.patch("/reactivar/:id", reactivarProyecto); // Solo cambia el estado de Proyecto a "activo". (Validacion 1: El que realiza la peticion debe tener tipo_usuario = administrador) (Validacion 2: El proyecto debe existir)
+/*Ajustar luego*/ router.post("/", authMiddleware, validarCrearProyecto, crearProyecto); // Crea un nuevo Proyecto (Recibe id_cliente, id_supervisor, nombre_proyecto, tipo_instalacion, direccion, nivel_exigencia, cantidad_personal_requerido) (Validacion 1: El que crea el proyecto debe tener tipo_usuario = administrador) (Validacion 2: El cliente id_cliente debe existir) (Validacion 3: El trabajador cuyo id_trabajador es id_supervisor debe existir) (Validacion 4: El trabajador cuyo id_trabajador es id_supervisor debe tener tipo_usuario = supervisor) (Validacion 4: El trabajador cuyo id_trabajador es id_supervisor no debe formar parte de otro proyecto (supervisar otro proyecto)) (Validacion 5: Los campos no deben estar vacios)
+router.get("/", authMiddleware, getAllProyectos); // (Entrega mediante su token trabajador_id) Retorna una lista de todos los proyectos del sistema, cada proyecto viene junto con informacion de su cliente, sus inventarios, y su informacion de contrato_proyecto junto a sus anexos, de la forma {"data": [.., {..proyecto, cliente: {..}, inventarios: [..], contrato_proyecto: {.., [..anexos_contrato_proyecto]}}], "meta": {total, page, limit, totalPages, orden, filtro}} (proyectos paginados y ordenados, orden alfabetico por nombre_proyecto por defecto, orden por fecha_creacion opcional, por defecto muestra todos los proyectos, pero permite filtro por estado_proyecto "activo" o "inactivo"). (Validacion 1: El que realiza la peticion debe tener en su token tipo_usuario = administrador)
+router.delete("/inactivar/:id_proyecto", authMiddleware, validarInactivarProyecto, inactivarProyecto); // Cambia el estado de un proyecto a inactivo, y el estado de sus cuadrillas a inactiva. (Recibe un id_proyecto) (Validacion 1: El id_proyecto debe existir) (Validacion 2: El proyecto debe estar activo) (Validacion 3: El que realiza la peticion debe tener tipo_usuario = administrador)
+router.patch("/reactivar/:id_proyecto", authMiddleware, validarReactivarProyecto, reactivarProyecto) //   Solo cambia el estado de un proyecto a activo. (Recibe un id_proyecto) (Validacion 1: El id_proyecto debe existir) (Validacion 2: El proyecto debe estar inactivo) (Validacion 3: El que realiza la peticion debe tener tipo_usuario = administrador) (Validacion 4: El id_supervisor del proyecto a reactivar tiene que ser diferente de null)
+router.patch("/supervisor/", validarCambiarSupervisorProyecto, cambiarSupervisorProyecto); // Cambia el id_supervisor de un proyecto, por el valor de un id_trabajador entregado en el body de la consulta (Recibe {id_proyecto, id_trabajador}) (Validacion 1: El id_proyecto debe existir) (Validacion 2: El id_trabajador debe existir) (Validacion 3: El id_trabajador debe tener tipo_usuario = supervisor) (Validacion 4: El id_trabajador no debe estar presente en el atributo id_supervisor de otro proyecto) (Validacion 5: El que realiza la peticion debe tener tipo_usuario = administrador)
+router.patch("/:id_proyecto", validarActualizarProyecto, actualizarProyecto); // Cambia datos generales del proyecto (Recibe {nombre_proyecto, tipo_instalacion, direccion, nivel_exigencia, cantidad_personal_requerido})  (Validacion 1: El id_proyecto debe existir) (Validacion 2: El proyecto debe estar activo) (Validacion 3: Se debe entregar al menos un campo a modificar) (Validacion 4: El que realiza la peticion debe tener tipo_usuario = administrador)
+router.delete("/supervisor/:id_proyecto", validarRemoverSupervisorDeProyecto, removerSupervisorDeProyecto); // Elimina el supervisor de un proyecto inactivo (Recibe token de autenticacion) (Validacion 1: El que realiza la consulta debe tener tipo_usuario = administrador) (Validacion 2: El proyecto debe existir) (Validacion 3: El proyecto debe tener estado = inactivo) (Validacion 4: El campo no puede estar vacio) (Validacion 5: El id_supervisor del proyecto debe ser distinto de null)
+
 
 // Trabajador & Supervisor
-/* LISTO */router.get("/misProyectos", getMyProyectosByToken); // Obtiene una lista paginada de los proyectos a los que pertenece un trabajador, en base a un id_trabajador proveido en el token.
-/* LISTO */router.get("/:id", getProyectData); // Informacion detallada del proyecto al clikearlo (Validar que pertenezca al proyecto || Sea Admin)
-
-
-/*Ajustar luego*/ router.post("/", authMiddleware, validarCrearProyecto, crearProyectoQuickFix); // Crea un nuevo Proyecto (Recibe id_cliente, id_supervisor, nombre_proyecto, tipo_instalacion, direccion, nivel_exigencia, cantidad_personal_requerido) (Validacion 1: El que crea el proyecto debe tener tipo_usuario = administrador) (Validacion 2: El cliente id_cliente debe existir) (Validacion 3: El trabajador cuyo id_trabajador es id_supervisor debe existir) (Validacion 4: El trabajador cuyo id_trabajador es id_supervisor debe tener tipo_usuario = supervisor) (Validacion 4: El trabajador cuyo id_trabajador es id_supervisor no debe formar parte de otro proyecto (supervisar otro proyecto)) (Validacion 5: Los campos no deben estar vacios)
+//router.get("/misProyectos", getMyProyectosByToken); // Obtiene una lista paginada de los proyectos a los que pertenece un trabajador, en base a un id_trabajador proveido en el token.
+//router.get("/:id", getProyectData); // Informacion detallada del proyecto al clikearlo (Validar que pertenezca al proyecto || Sea Admin)
 
 
 export default router;
